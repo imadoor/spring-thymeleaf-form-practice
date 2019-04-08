34b3df841dc0d1c1ce5970e9f3585122cc868cb2
Amador Ollarzabal <amador.ollarzabal@outlook.com>
2019-04-07 19:47:44 -0500

    updated the angularjs piece

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,10 +1,25 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Greeting {
 
     private long id;
     private String content;
 
+    List<String> list = new ArrayList<>();
+
+  //Return copy of the list
+  public List<String> getList() {
+      return new ArrayList<>(list);
+  }
+  
+  public void setList(List<String> listToBeSet) {
+	    if (listToBeSet != null)
+	        this.list.addAll(listToBeSet);
+	}
+    
     public long getId() {
         return id;
     }
